d51797ea6bb7eabccaff6d6697605503ac158a04
Sibonelo Mazibuko <mazibukosibonelo@gmail.com>
2025-06-28 01:26:28 +0200

    Update hello_world.cs

--- fundamentals/hello-world/hello_world.cs
+++ fundamentals/hello-world/hello_world.cs
@@ -1,1 +1,13 @@
+//Prints 'Hello World!' on the screen
+Using System.Console;
 
+namespace HelloWorld
+{
+  class Program
+  {
+    public static void Main(string [] args)
+    {
+      Console.Write("Hello World!");
+    }
+  }
+}
