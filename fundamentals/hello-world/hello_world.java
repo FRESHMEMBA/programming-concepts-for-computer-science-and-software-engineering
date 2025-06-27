fe57be7bebd6efa6dc8c1feae658c2831d6729f2
Sibonelo Mazibuko <mazibukosibonelo@gmail.com>
2025-06-28 01:32:23 +0200

    Update hello_world.java

--- fundamentals/hello-world/hello_world.java
+++ fundamentals/hello-world/hello_world.java
@@ -1,1 +1,8 @@
+//Prints 'Hello World' on the screen
+import java.util.System;
 
+public class hello_world {
+  public static void main(String [] args) {
+    System.out.print("Hello World!");
+  }
+}
