1a8d0c60f3e3b238b70643611e7cdd29905af3bb
FRESHMEMBA <mazibukosibonelo@gmail.com>
2025-12-30 17:31:38 +0200

    successful build for DataTypes_JAVA

--- fundamentals/2_data-types/DataTypes_JAVA/DataTypes.java
+++ fundamentals/2_data-types/DataTypes_JAVA/DataTypes.java
@@ -1,1 +1,23 @@
+// Declares some variables related to the details of a particular individual.
+// This includes name, surname, age, height, weight, gender, and marital status.
+// Then prints the values of these variables on the console.
 
+class DataTypes {
+    public static void main(String [] args) {
+        String name = "Jane";
+        String surname = "Doe";
+        int age = 33;
+        float height = 125.6f;  // cm
+        double weight = 72.5;  // kg
+        char gender = 'f';  // female
+        boolean married = false;
+
+        System.out.println("Name: " + name);
+        System.out.println("Surname: " + surname);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Weight: " + weight);
+        System.out.println("Gender: " + gender);
+        System.out.println("Married: " + married);
+    }
+}
