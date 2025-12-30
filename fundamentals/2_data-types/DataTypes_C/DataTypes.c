58ea83f174a5b8b79f887c7dc5531852eb9c7ecb
FRESHMEMBA <mazibukosibonelo@gmail.com>
2025-12-30 17:09:06 +0200

    successful build for DataType.c

--- fundamentals/2_data-types/DataTypes_C/DataTypes.c
+++ fundamentals/2_data-types/DataTypes_C/DataTypes.c
@@ -1,1 +1,27 @@
+// Declares some variables related to the details of a particular individual.
+// This includes name, surname, age, height, weight, gender, and marital status.
+// Then prints the values of these variables on the console.
 
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    char name[] = "Jane";
+    char surname[] = "Doe";
+    int age = 33;
+    float height = 125.6f;  // cm
+    double weight = 72.5;  // kg
+    char gender = 'F';  // female
+    bool married = false;
+
+    printf("Name: %s\n", name);
+    printf("Surname: %s\n", surname);
+    printf("Age: %d\n", age);
+    printf("Height: %.2f\n", height);
+    printf("Weight: %.2f\n", weight);
+    printf("Gender: %c\n", gender);
+    printf("Married: %s\n", married ? "true" : "false");
+    
+    return 0;
+}
