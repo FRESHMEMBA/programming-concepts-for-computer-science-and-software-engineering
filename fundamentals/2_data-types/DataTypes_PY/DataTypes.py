31ae072390dd1db2b013488b984fa33da3ab0862
FRESHMEMBA <mazibukosibonelo@gmail.com>
2025-12-30 17:49:10 +0200

    successful build for DataTypes_PY

--- fundamentals/2_data-types/DataTypes_PY/DataTypes.py
+++ fundamentals/2_data-types/DataTypes_PY/DataTypes.py
@@ -1,1 +1,19 @@
+# Declares some variables related to the details of a particular individual.
+# This includes name, surname, age, height, weight, gender, and marital status.
+# Then prints the values of these variables on the console.
 
+name = "Jane"
+surname = "Doe"
+age = 33
+height = 125.6  # cm
+weight = 72.5  # kg
+gender = 'f'    # female
+married = False
+
+print("Name:", name)
+print("Surname:", surname)
+print("Age:", age)
+print("Height:", height)
+print("Weight:", weight)
+print("Gender:", gender)
+print("Married:", married)
